226ad02620f4b40aa61c2271ba5b1b4280d262eb
michaelhyun <michael.hyun3@gmail.com>
2018-01-17 03:00:40 -0800

    Added Navigation bar Icons
    Added icons with titles, no funciotnality yet.

---
+++ BuckIt/BuckIt/BucketListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BucketListViewController.swift
+//  BuckIt
+//
+//  Created by Michael Hyun on 1/16/18.
+//  Copyright © 2018 Samnang Sok. All rights reserved.
+//
+
+import UIKit
+
+class BucketListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
