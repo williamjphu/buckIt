e0e5ac29d5ca5489f334c0a333225700271c1a43
Samnang Sok <samnangsok@Samnangs-MacBook-Pro.local>
2018-01-21 23:52:21 -0800

    follower and following ui

--- BuckIt/BuckIt/ProfileViewController.swift
+++ BuckIt/BuckIt/ProfileViewController.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-@IBDesignable
+
 class ProfileViewController: UIViewController {
 
     
@@ -17,20 +17,5 @@ class ProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
