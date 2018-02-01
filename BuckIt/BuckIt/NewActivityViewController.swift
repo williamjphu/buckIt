8549cad01598f7a4f2830578ad5ae2747d5a1aa5
michaelhyun <michael.hyun3@gmail.com>
2018-02-01 01:33:56 -0800

    UI almost done for New Activity
    Finished most of the UI, but still needs functionality and start
    date and end date.

--- BuckIt/BuckIt/NewActivityViewController.swift
+++ BuckIt/BuckIt/NewActivityViewController.swift
@@ -7,15 +7,35 @@
 //
 
 import UIKit
+import MapKit
+import UITextView_Placeholder
 
-class NewActivityViewController: UIViewController {
+class NewActivityViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var titleText: UITextView!
+    @IBOutlet weak var profilePic: UIImageView!
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTitleTextArea()
     }
-
+    
+    func setupTitleTextArea(){
+        titleText.delegate = self
+        titleText.placeholder = "Add a Title..."
+        
+        descriptionText.delegate = self
+        descriptionText.placeholder = "Add a Description..."
+        
+        profilePic.layer.borderWidth = 1
+        profilePic.layer.masksToBounds = false
+        profilePic.layer.cornerRadius = self.profilePic.frame.height/2
+        profilePic.clipsToBounds = true
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
