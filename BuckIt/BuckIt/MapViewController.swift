21efae851a56ef2439f3ab153841b5b89c9f07d5
Will <jiepeng.huwu@sjsu.edu>
2018-02-19 14:10:59 -0800

    new map API

--- BuckIt/BuckIt/MapViewController.swift
+++ BuckIt/BuckIt/MapViewController.swift
@@ -6,17 +6,17 @@
 //  Copyright © 2018 Samnang Sok. All rights reserved.
 //
 
-import UIKit
-import CoreLocation
-import MapKit
+import Mapbox
 
-class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class MapViewController: UIViewController, MGLMapViewDelegate {
 
-    @IBOutlet weak var mapView: MKMapView!
-    var locationManager : CLLocationManager!
+//    @IBOutlet weak var mapView: MKMapView!
+//    var locationManager : CLLocationManager!
     //var initialLocation : CLLocation!
     
-    var mapHasCenteredOnce = false
+//    var mapHasCenteredOnce = false
+    var mapboxView: MGLMapView!
+//    var directionsRoute: Route?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,9 +29,25 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
 //        mapView.userTrackingMode = MKUserTrackingMode.follow
 //        initialLocation = CLLocation(latitude: 37.33467, longitude: -121.87533)
 //        centerMapOnLocation(location: initialLocation)
-        determineCurrentLocation()
+//        determineCurrentLocation()
+        
+        view.addSubview(mapboxView)
+        mapboxView = MGLMapView(frame: view.bounds)
+        
+//        mapboxView.delegate = self
+//        mapboxView.showsUserLocation = true
+//        mapboxView.userTrackingMode(.follow, animated: true)
+        //mapboxView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        //mapboxView.setCenter(CLLocationCoordinate2D(latitude: 37.33, longitude: 121.88), zoomLevel: 9, animated: false)
+        //view.addSubview(mapboxView)
+//        let userLocation : CLLocation = locations[0] as CLLocation
+      
+        
     }
     
+//    func mapViewDidFinishLoadingMap(_ mapView: MGLMapView) {
+//        mapboxView.setCenter((mapboxView.userLocation?.coordinate)!, animated: false)
+//    }
 //    func locationAuthStatus() {
 //        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
 //            mapView.showsUserLocation = true
@@ -61,19 +77,19 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
 //        }
 //    }
     
-    func determineCurrentLocation() {
-        locationManager = CLLocationManager()
-        locationManager.delegate = self
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        
-        if CLLocationManager.locationServicesEnabled() {
-            locationManager.startUpdatingLocation()
-        }
-    }
-    
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
+//    func determineCurrentLocation() {
+//        locationManager = CLLocationManager()
+//        locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestWhenInUseAuthorization()
+//        
+//        if CLLocationManager.locationServicesEnabled() {
+//            locationManager.startUpdatingLocation()
+//        }
+//    }
+//
+//    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+//        print(locations)
 //        let location = locations[locations.count - 1]
 //        if location.horizontalAccuracy > 0 {
 //            locationManager.stopUpdatingLocation()
@@ -84,20 +100,24 @@ class MapViewController: UIViewController, CLLocationManagerDelegate, MKMapViewD
 //            let currentLoc = CLLocation(latitude: lat, longitude: lon)
 //            centerMapOnLocation(location: currentLoc)
 //        }
-        let userLocation : CLLocation = locations[0] as CLLocation
-        locationManager.stopUpdatingLocation()
-        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
-        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-        mapView.setRegion(region, animated: true)
         
-        let annotation : MKPointAnnotation = MKPointAnnotation()
-        annotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
-        annotation.title = "Current location"
-        mapView.addAnnotation(annotation)
-    }
+//        let userLocation : CLLocation = locations[0] as CLLocation
+        
+        
+//        locationManager.stopUpdatingLocation()
+//        let center = CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude)
+//        let region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+//        mapView.setRegion(region, animated: true)
+        
+        
+//        let annotation : MKPointAnnotation = MKPointAnnotation()
+//        annotation.coordinate = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
+//        annotation.title = "Current location"
+//        mapView.addAnnotation(annotation)
+//    }
     
     //Write the didUpdateLocations method here: such as airplane mode, or no tracking allows
-    func locationManager(_ manager: CLLocationManager, didFailWithError error: NSError) {
-        print("Error \(error)")
-    }
+//    func locationManager(_ manager: CLLocationManager, didFailWithError error: NSError) {
+//        print("Error \(error)")
+//    }
 }
