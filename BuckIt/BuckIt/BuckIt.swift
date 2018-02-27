e1cda0e15b1b77cf76df48fd1c32b9d692a73520
michaelhyun <michael.hyun3@gmail.com>
2018-02-27 11:54:53 -0800

    Added FetchBucket method in the profile page

---
+++ BuckIt/BuckIt/BuckIt.swift
@@ -0,0 +1,17 @@
+//
+//  BuckIt.swift
+//  BuckIt
+//
+//  Created by Michael Hyun on 2/26/18.
+//  Copyright © 2018 Samnang Sok. All rights reserved.
+//
+
+import UIKit
+
+class BuckIt : NSObject {
+    var buckitId : String?
+    var title : String?
+    var desc : String?
+    var pathToImage : String?
+    var userId : String?
+}
