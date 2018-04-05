5c839dd3d76042fc0e09dd910665039ff91c13a7
samnang sok <samnangsok.se@gmail.com>
2018-04-04 20:25:17 -0700

    the profile buckit is clickable and send it to buckit list page

---
+++ BuckIt/BuckIt/BuckitCell.swift
@@ -0,0 +1,17 @@
+//
+//  BuckitCell.swift
+//  BuckIt
+//
+//  Created by Samnang Sok on 4/4/18.
+//  Copyright © 2018 Samnang Sok. All rights reserved.
+//
+
+import UIKit
+
+class BuckitCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var buckitImage: UIImageView!
+    
+    @IBOutlet weak var BuckitName: UILabel!
+}
